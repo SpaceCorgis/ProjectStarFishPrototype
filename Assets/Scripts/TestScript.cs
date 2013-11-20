7655d925520658a017ce382b75ff1dddcd618032
TJDabs <tjdaberkow@gmail.com>
2013-11-19 19:54:48 -0600

    Created Stock Scene with animations

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -5,12 +5,25 @@ public class TestScript : MonoBehaviour {
 
     //added stuff from VS
 	// Use this for initialization
+
+    private Animator anim;
+
 	void Start () {
+        anim = GetComponent<Animator>();
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("Hit Button");
+            anim.SetTrigger("Pushed");
+        }
+        if (Input.GetButtonUp("Jump"))
+        {
+            anim.SetTrigger("Pushed");
+        }
 	
 	}
 }
